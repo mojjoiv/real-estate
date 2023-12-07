367753922367084c7bd24f7bb28f7442c09f536e
mojjoiv <daltonongeche@gmail.com>
2023-12-07 22:14:41 +0300

    login and signup fixed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,13 @@
 import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
   try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json('User created successfully!');
   } catch (error) {
@@ -16,14 +16,26 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
-  const { email, password } = req.body;
   try {
+    const { email, password } = req.body;
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, 'User not found!'));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found!'));
+    }
+
+    const validPassword = await bcrypt.compare(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, 'Wrong credentials!'));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h', // Set expiration time for the token
+    });
+
     const { password: pass, ...rest } = validUser._doc;
+
     res
       .cookie('access_token', token, { httpOnly: true })
       .status(200)
@@ -33,32 +45,49 @@ export const signin = async (req, res, next) => {
   }
 };
 
+// Function to generate a random password
+const generateRandomPassword = () => {
+  return (
+    Math.random().toString(36).slice(-8) +
+    Math.random().toString(36).slice(-8)
+  );
+};
+
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
+
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '1h', // Set expiration time for the token
+      });
+
       const { password: pass, ...rest } = user._doc;
+
       res
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
     } else {
-      const generatedPassword =
-        Math.random().toString(36).slice(-8) +
-        Math.random().toString(36).slice(-8);
-      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const generatedPassword = generateRandomPassword();
+      const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+
       const newUser = new User({
-        username:
-          req.body.name.split(' ').join('').toLowerCase() +
+        username: req.body.name.split(' ').join('').toLowerCase() +
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
       });
+
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: '1h', // Set expiration time for the token
+      });
+
       const { password: pass, ...rest } = newUser._doc;
+
       res
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
